83926b15434e43d3d182baa999b94a406ebd9c76
HIJRATULGEBI PRAMANA <42725375+HijratulgebiPramana@users.noreply.github.com>
2018-09-10 09:50:51 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,4 +1,46 @@
-<?php 
-//Tuliskan kode anda di bawah ini
+<?php
+	function rekursif($rek){
+		global $i;
 
-?>
+		rekursif2($i, $rek);
+		echo "<br>";
+		if(--$rek > $i){
+			rekursif($rek);
+		}
+	}
+	function rekursif2($j, $rek){
+		global $i;
+
+		if($j>$i -$rek ){
+			echo "*";
+			rekursif2($j-1, $rek);
+		}
+	}
+	$inputkanr = $_GET["inputkan"];
+	$i = 0;
+	rekursif($inputkanr);
+
+	//=================================================
+
+	for ($i=1; $i <=$inputkanr ; $i++) {
+		if($i %2 ==0){
+			$genap[]=$i;
+		} else {
+			$ganjil[]=$i;
+		}
+	}	
+	echo "genap = ";
+	foreach ($genap as $bil_genap) {
+		echo $bil_genap. ',';
+		
+	}
+	echo "<br>";
+	echo "ganjil";
+	foreach ($ganjil as $bil_ganjil) {
+		echo $bil_ganjil . ',';
+		
+	}
+	echo "<br> jumlah deret genap =". count($genap) . "<br>";
+	echo "jumlah deret ganjil =". count($ganjil);
+
+	?>
